8b067be3e7410a527a138cef97cf0689859ceef3
Hubits-Cloud <Tobias.Belling@hotmail.com>
2023-12-07 10:45:57 +0100

    updated all files and added a jpeg

--- trailer.js
+++ trailer.js
@@ -1,14 +1,15 @@
 const trailer = document.getElementById("trailer");
 
-window.onmousemove = event => {
-    const x = event.clientX - trailer.offsetWidth / 2,
-          y = event.clientY - trailer.offsetHeight / 2;
+window.onmousemove = e => {
+    const x = e.clientX - trailer.offsetWidth / 2,
+          y = e.clientY - trailer.offsetHeight / 2;
     
     const keyframes = {
         transform: `translate(${x}px, ${y}px)`
     }
 
     trailer.animate(keyframes, {
-        duration: 800
+        duration: 800,
+        fill: "forwards"
     });
 }
